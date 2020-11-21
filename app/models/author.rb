4675a3ca8b256c533098d365131f4aa636fbcc89
ashill99 <admshng@gmail.com>
2020-11-20 19:21:39 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+
+    validates :email, uniqueness: true 
+    validates :name, presence: true 
+    validates :phone_number, length: { maximum: 10, minimum: 10 }
 end
